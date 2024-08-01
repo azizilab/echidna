0e66392a26191243fc1f41be7346ec9e05cf4515
Josh Myers <joshm1238@gmail.com>
2024-07-31 23:56:51 -0400

    added early stopping util

--- echidna/utils.py
+++ echidna/utils.py
@@ -110,3 +110,73 @@ def prepare_input(X, W, sample_name, timepoints, n_subsamples, device, clst_alg=
 
 
 
+def __prepare_input(X, W, sample_name, timepoints, num_clusters, device):
+
+    # format single-timestep input
+    if len(timepoints) == 1:
+        X_obs = torch.from_numpy(X.layers['counts']).to(device)
+        condition = sample_name + "_" + timepoints[0] + "_" + "count"
+        W = W[condition]
+        W_obs = torch.from_numpy(W.values).to(torch.float32).to(device)
+        z_obs_series = X.obs['leiden'].values.astype(int)
+        pi_obs_series = np.unique(z_obs_series, return_counts=True)[1] / len(z_obs_series)
+        z_obs = torch.from_numpy(np.array(z_obs_series)).to(torch.int32).to(device)
+        pi_obs = torch.from_numpy(pi_obs_series).to(torch.float32).to(device)
+    
+    # format multi-timestep input
+    else:
+        timepoints = sorted(X.obs['treatment'].unique())
+        t1 = X[X.obs['condition'] == timepoints[0]].shape[0]
+        t2 = X[X.obs['condition'] == timepoints[1]].shape[0]
+        n_subsamples = min(t1, t2)
+
+        Xs, Zs, Pis = [], [], []
+        conditions = []
+        for timepoint in timepoints:
+            condition = sample_name + "_" + timepoint + "_" + "count"
+            conditions.append(condition)
+            if X[X.obs['condition'] == timepoint].shape[0] == 0:
+                raise ValueError("One of more timepoints do not exist in this sample")
+            
+            # get X at a timepoint
+            X_t = sc.pp.subsample(X[X.obs['condition'] == timepoint], n_obs=n_subsamples, copy=True)
+            X_t_obs = torch.from_numpy(X_t.layers['counts']).to(torch.float32).to(device)
+
+            # get z at a timepoint
+            z_t = X_t.obs['leiden'].values.astype(int)
+            z_obs_t = torch.from_numpy(np.array(z_t)).to(torch.int32).to(device)
+
+            # get pi at a timepoint
+            pi_obs_t = format_pi_one_timepoint(X, timepoint, num_clusters)
+
+            Zs.append(z_obs_t)
+            Xs.append(X_t_obs)
+            Pis.append(pi_obs_t)
+        X_obs = torch.stack(Xs)
+        z_obs = torch.stack(Zs)
+        pi_obs = torch.stack(Pis)
+        W_obs = torch.from_numpy(W[conditions].values.T).to(torch.float32).to(device)
+    return X_obs, W_obs, z_obs, pi_obs
+
+
+# prepare input for echidna
+def prepare_input_early_stopping(X, W, sample_name, timepoints, device, train_ratio=0.9, seed=0):
+    torch.set_default_dtype(torch.float32)
+    torch.set_default_device(device)
+
+    matched_genes = X.var.index.intersection(W.index)
+    W = W.loc[matched_genes]
+    X = X[:, matched_genes]
+    
+    num_clusters = len(np.unique(np.array(X.obs['leiden'].values)))
+
+    idx = np.arange(0, len(X))
+    np.random.seed(seed)
+    np.random.shuffle(idx)
+
+    X = X[idx]
+
+    train_size = int(train_ratio*len(X))
+
+    return __prepare_input(X[:train_size], W, sample_name, timepoints, num_clusters, device), \
+           __prepare_input(X[train_size:], W, sample_name, timepoints, num_clusters, device)
